f8bbc44d8063cd0ece4e024abeca3165bc0d52e4
KarimIbrahim11 <karim.ibrahim.amin@gmail.com>
2021-01-16 20:05:46 +0200

    Feature 5 Added

--- main.py
+++ main.py
@@ -22,15 +22,14 @@ scanned_components, scanned_Lines = linesComponents(scanned_binary, img.shape[1]
 handwritten_components, handwritten_Lines = linesComponents(handwritten_binary, img.shape[1])
 show_images(scanned_Lines)
 
-f1 = 0
 sum_scanned = 0
 sum_handwritten = 0
 avgs_length_scanned = []
 avgs_height_scanned = []
 avgs_length_handwritten = []
 avgs_height_handwritten = []
-# avgs_blobs_scanned = []
 avgs_blobs_handwritten = []
+
 # Retrieving Words Images and Components from each line
 
 print("number of Scanned lines", len(scanned_Lines))
@@ -50,22 +49,41 @@ for i in range(len(scanned_Lines)):
 sum_scanned /= len(scanned_Lines)
 avg_length_scanned = np.average(avgs_length_scanned)
 avg_height_scanned = np.average(avgs_height_scanned)
-# avgs_blobs_scanned = np.array(avgs_blobs_scanned)
+average_gaps_line = []
 print("number of handwritten lines", len(handwritten_Lines))
-for i in range(len(handwritten_Lines)):  # line in handwritten_Lines:
+max_col = 0
+min_col = 0
+for i in range(len(handwritten_Lines)):
     handwritten_words_components, handwritten_arrayOfWords, handwritten_words_boxes = wordsComponents(
         handwritten_Lines[i])
-    words_images = segmentBoxesInImage(handwritten_words_boxes, handwritten_Lines[i], False)
-    words_blobs_areas = [blobArea(words_images[j] ^ segmentation.flood_fill(words_images[j], (0, 0), 255)) for j in \
-                         range(words_images.shape[0])]
-
-    npsum = np.sum(handwritten_words_boxes, axis=0)
+    # f5
+    words_gaps = []
+    words_blobs_areas = []
+    if handwritten_arrayOfWords.shape[0] > 1:
+        for j in range(handwritten_arrayOfWords.shape[0]):
+            # f5
+            if j != 0:
+                words_gaps.append(handwritten_words_boxes[j][1] - max_col)
+            max_col = handwritten_words_boxes[j][3]
+            # f4
+            words_blobs_areas.append(blobArea(
+                handwritten_arrayOfWords[j] ^ segmentation.flood_fill(handwritten_arrayOfWords[j], (0, 0), 255)))
+        average_gaps_line.append(np.average(words_gaps))
+        avgs_blobs_handwritten.append(np.average(words_blobs_areas))
+    # f4 calculations
+    # words_blobs_areas = [
+    #    blobArea(handwritten_arrayOfWords[j] ^ segmentation.flood_fill(handwritten_arrayOfWords[j], (0, 0), 255)) for j
+    #    in \
+    #    range(handwritten_arrayOfWords.shape[0])]
+    # f1
     length_handwritten_words = len(handwritten_arrayOfWords)
-    avgs_length_handwritten.append((npsum[3] - npsum[1]) / length_handwritten_words)
-    avgs_height_handwritten.append((npsum[2] - npsum[0]) / length_handwritten_words)
-    avgs_blobs_handwritten.append(np.average(words_blobs_areas))
     sum_handwritten += length_handwritten_words
-
+    # f2
+    npsum = np.sum(handwritten_words_boxes, axis=0)
+    avgs_length_handwritten.append((npsum[3] - npsum[1]) / length_handwritten_words)
+    # f3
+    avgs_height_handwritten.append((npsum[2] - npsum[0]) / length_handwritten_words)
+print("words gaps:", average_gaps_line)
 sum_handwritten /= len(handwritten_Lines)
 avg_length_handwritten = np.average(avgs_length_handwritten)
 avg_height_handwritten = np.average(avgs_height_handwritten)
@@ -75,6 +93,8 @@ f1 = sum_scanned / sum_handwritten
 f2 = avg_length_scanned / avg_length_handwritten
 f3 = avg_height_scanned / avg_height_handwritten
 f4 = np.average(avgs_blobs_handwritten)
+f5 = np.average(average_gaps_line)
+print("f5", f5)
+print("f4", f4)
 
-
-print(f4)
+print(f3)
